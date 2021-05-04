c54972ba8bba0150a84fb3f3ef21a3388d26a115
zengyu <zeng.yu.1993@gmail.com>
2021-05-04 13:25:22 +0800

    [ADD] 前端分页选择器

--- src/components/LedgerMonthList.js
+++ src/components/LedgerMonthList.js
@@ -100,6 +100,81 @@ const monthLedgerViewStyle = css`
     }
 `;
 
+const pageSelectorStyle = css`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 2px 5px;
+        min-width: 18px;
+        border: 1px solid #ecf0f1;
+        user-select: none;
+        cursor: pointer;
+
+        :hover {
+            background-color: #ecf0f1;
+        }
+        :active {
+            background-color: #bdc3c7;
+        }
+
+        &.current {
+            background-color: #ecf0f1;
+        }
+    }
+`;
+
+const PageSelector = ({onChange, size, total}) => {
+
+    const [current, setCurrent] = useState(1);
+
+    useEffect(() => {
+        onChange({limit: size, offset: (current - 1) * size})
+    }, [current, onChange, size, total])
+
+    const fillPage = (size, total) => {
+        // 计算总页数，向上取整，不能填满首页的补足首页
+        let cnt = Math.ceil(total / size);
+        if (cnt === 0) {
+            cnt = 1;
+        }
+
+        // 确定滑窗位置
+        const head = Math.min(Math.max(1, current - 2), cnt - 4);
+        const tail = Math.max(Math.min(cnt, current + 2), 5);
+
+        // 渲染页码
+        // 开头的是向前翻页按钮
+        const index = [];
+        index.push(<div key={'prev'} onClick={() => setCurrent(current === 1 ? current : current - 1)}>{'<'}</div>)
+
+        if (head > 1) {
+            index.push(<div key={'prev-hidden'} onClick={() => setCurrent(Math.max(1, current - 5))}>{'\u22ef'}</div>)
+        }
+        for (let i = head; i <= tail; i++) {
+            index.push(<div className={i === current ? 'current' : ''} onClick={() => setCurrent(i)} key={i}>{i}</div>);
+        }
+        if (tail < cnt) {
+            index.push(<div key={'next-hidden'} onClick={() => setCurrent(Math.min(cnt, current + 5))}>{'\u22ef'}</div>)
+        }
+
+        // 向后翻页按钮
+        index.push(<div key={'next'} onClick={() => setCurrent(current === cnt ? current : current + 1)}>{'>'}</div>)
+        
+        return index;
+    };
+
+    return (
+        <div className={pageSelectorStyle}>
+            {fillPage(size, total)}
+        </div>
+    )
+}
+
 const LedgerMonthList = (props) => {
     const [items, setItems] = useState([]);
     const [categories, setCategories] = useState([]);
@@ -136,6 +211,7 @@ const LedgerMonthList = (props) => {
                         })}
                     </tbody>
                 </table>
+                <PageSelector onChange={p => console.log(p)} size={5} total={101}/>
             </main>
         </div>
     )
